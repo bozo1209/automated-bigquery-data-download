8cca36428205c241188a8f21411e58a8fbd61eb0
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2023-10-29 00:02:22 +0200

    Initial commit

---
+++ src/main/java/com/bozo/automatedbigquerydatadownload/AutomatedBigqueryDataDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.bozo.automatedbigquerydatadownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedBigqueryDataDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedBigqueryDataDownloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bozo/automatedbigquerydatadownload/AutomatedBigqueryDataDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bozo.automatedbigquerydatadownload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedBigqueryDataDownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
