8eb6cd2c0a747642feb550c39b6db87157eb4d0b
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2023-11-11 20:16:32 +0100

    add endpoint for covid cases japan in bigquery controller closes #8

--- src/main/java/com/bozo/automatedbigquerydatadownload/controller/BigQueryController.java
+++ src/main/java/com/bozo/automatedbigquerydatadownload/controller/BigQueryController.java
@@ -1,10 +1,26 @@
 package com.bozo.automatedbigquerydatadownload.controller;
 
 import com.bozo.automatedbigquerydatadownload.service.BigQueryService;
+import com.google.cloud.bigquery.TableResult;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BigQueryController {
 
     private BigQueryService bigQueryService;
+
+    @GetMapping("/covid_cases_japan")
+    public String covidCasesJapan(Model model){
+        TableResult covidCasesJapan;
+        try {
+            covidCasesJapan = bigQueryService.covidCasesJapan();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        model.addAttribute("tableHeaders", covidCasesJapan.getSchema().getFields());
+        model.addAttribute("tableValues", covidCasesJapan.getValues());
+        return "covidCasesJapan";
+    }
 }
