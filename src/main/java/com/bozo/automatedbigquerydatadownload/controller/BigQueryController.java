583eb8002aa94fa594dc0813b307a3b0a11bc733
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2023-11-11 20:06:26 +0100

    create bigquery controller closes #7

---
+++ src/main/java/com/bozo/automatedbigquerydatadownload/controller/BigQueryController.java
@@ -0,0 +1,10 @@
+package com.bozo.automatedbigquerydatadownload.controller;
+
+import com.bozo.automatedbigquerydatadownload.service.BigQueryService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BigQueryController {
+
+    private BigQueryService bigQueryService;
+}
