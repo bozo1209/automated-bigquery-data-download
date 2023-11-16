916a15acf1a34fc0ad9e07ac274b9b83cd52892b
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2023-11-16 01:55:27 +0100

    fix sql injection vulnerability closes #26

--- src/main/java/com/bozo/automatedbigquerydatadownload/service/BigQueryService.java
+++ src/main/java/com/bozo/automatedbigquerydatadownload/service/BigQueryService.java
@@ -3,10 +3,12 @@ package com.bozo.automatedbigquerydatadownload.service;
 import com.bozo.automatedbigquerydatadownload.enums.Query;
 import com.google.cloud.bigquery.BigQuery;
 import com.google.cloud.bigquery.QueryJobConfiguration;
+import com.google.cloud.bigquery.QueryParameterValue;
 import com.google.cloud.bigquery.TableResult;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.TemplateEngine;
+
+import java.util.Map;
 
 @AllArgsConstructor
 @Service
@@ -23,11 +25,32 @@ public class BigQueryService {
                 );
     }
 
+    public TableResult runQuery(String query, Map<String, Object> parameterValueMap) throws InterruptedException{
+        return bigQuery.query(addNamedParameters(query, parameterValueMap));
+    }
+
+    private QueryJobConfiguration addNamedParameters(String query, Map<String, Object> parameterValueMap){
+        QueryJobConfiguration.Builder builder = QueryJobConfiguration.newBuilder(query);
+
+        parameterValueMap.forEach((k, v) -> {
+            if (v instanceof String){
+                System.out.println("congratulation! It is String");
+                builder.addNamedParameter(k, QueryParameterValue.string((String) v));
+            } else if (v instanceof String[]) {
+                System.out.println("congratulation! It is String[]");
+                builder.addNamedParameter(k, QueryParameterValue.array((String[]) v, String.class));
+            }
+        });
+
+
+        return builder.build();
+    }
+
     public TableResult covidCasesJapan() throws InterruptedException {
         return runQuery(Query.JAPAN_COVID.getQuery());
     }
 
     public TableResult covidCasesJapanWithPrefecture(String prefecture) throws InterruptedException{
-        return runQuery(Query.JAPAN_COVID_PREFECTURE.getQuery().replaceAll("\\$\\{prefectureName\\}", prefecture));
+        return runQuery(Query.JAPAN_COVID_PREFECTURE.getQuery(), Map.of("prefecture_name", prefecture));
     }
 }
