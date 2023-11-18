d37caa7fbecdce7cf6bc2eeabb0cce0513a6ea2c
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2023-11-18 23:43:04 +0100

    add query with 'where' keyword for list of values closes #15

--- src/main/java/com/bozo/automatedbigquerydatadownload/enums/Query.java
+++ src/main/java/com/bozo/automatedbigquerydatadownload/enums/Query.java
@@ -2,7 +2,8 @@ package com.bozo.automatedbigquerydatadownload.enums;
 
 public enum Query {
     JAPAN_COVID("SELECT * FROM `bigquery-public-data.covid19_public_forecasts.japan_prefecture_28d` LIMIT 10;"),
-    JAPAN_COVID_PREFECTURE("SELECT * FROM `bigquery-public-data.covid19_public_forecasts.japan_prefecture_28d` where prefecture_name = @prefecture_name LIMIT 10;");
+    JAPAN_COVID_PREFECTURE("SELECT * FROM `bigquery-public-data.covid19_public_forecasts.japan_prefecture_28d` WHERE prefecture_name = @prefecture_name LIMIT 10;"),
+    JAPAN_COVID_PREFECTURES("SELECT * FROM `bigquery-public-data.covid19_public_forecasts.japan_prefecture_28d` WHERE prefecture_name IN UNNEST(@prefecture_names) LIMIT 10;");
 
     private final String query;
 
